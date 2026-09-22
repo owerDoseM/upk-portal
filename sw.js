352442d119f35ed9cf0a9804f064e23fcff20cf1
owerDoseM <arsenpetrosyan9108@gmail.com>
2026-09-22 19:45:38 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME='upk-portal-v4';
+const CACHE_NAME='upk-portal-v5';
 const ASSETS=[
   './',
   './index.html',
-  './style.css?v=3',
-  './app.js?v=3',
+  './style.css?v=5',
+  './app.js?v=5',
   './manifest.json',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js',
   'https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js'
